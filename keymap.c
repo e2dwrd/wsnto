e23290c012a5c62a4374e2aff3d326896add43e1
e2dwrd <114947003+e2dwrd@users.noreply.github.com>
2023-06-08 19:34:32 +0500

    Add files via upload

---
+++ keymap.c
@@ -0,0 +1,84 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┐
+     * │ 7 │ 8 │ 9 │ / │
+     * ├───┼───┼───┼───┤
+     * │ 4 │ 5 │ 6 │ * │
+     * ├───┼───┼───┼───┤
+     * │ 1 │ 2 │ 3 │ - │
+     * ├───┼───┼───┼───┤
+     * │ 0 │ . │Ent│ + │
+     * └───┴───┴───┴───┘
+     */
+    [0] = LAYOUT_2x4_2(
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS
+    ),
+
+    [1] = LAYOUT_2x4_2(
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS
+    ),
+
+    [2] = LAYOUT_2x4_2(
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS
+    ),
+
+    [3] = LAYOUT_2x4_2(
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS  
+    ),
+
+    [4] = LAYOUT_2x4_2(
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS   
+    ),
+
+    [5] = LAYOUT_2x4_2(
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS  
+    ),
+
+    [6] = LAYOUT_2x4_2(
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS 
+    ),
+
+    [7] = LAYOUT_2x4_2(
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS 
+    ),
+
+    [8] = LAYOUT_2x4_2(
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+        KC_TRNS,   KC_TRNS  
+    )
+};
+
+         #if defined(ENCODER_MAP_ENABLE)
+    const uint16_t PROGMEM encoder_map[][1][2] = {
+    [0] =  {  ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
+    [1] =  {  ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
+    [2] =  {  ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
+    [3] =  {  ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
+    [4] =  {  ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
+    [5] =  {  ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
+    [6] =  {  ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
+    [7] =  {  ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  },
+    [8] =  {  ENCODER_CCW_CW(KC_TRNS, KC_TRNS)  }
+     };
+    #endif
+
+
--- rules.mk
+++ rules.mk
@@ -1,6 +1,4 @@
 # This file intentionally left blank
-ENCODER_ENABLE = yes
-BOOTLOADER = rp2040
-EXTRAKEY_ENABLE = yes
-MOUSEKEY_ENABLE = yes
-NKRO_ENABLE = yes
+VIA_ENABLE  = yes
+VIAL_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
